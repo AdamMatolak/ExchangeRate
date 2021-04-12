9ec4f93c868f571a642499e619fc680bd147e01f
AdamMatolak <adam.matolak@kosickaakademia.sk>
2021-04-12 20:00:25 +0200

    Updated Calc.java

--- src/main/java/sk/kosickaakademia/matolak/exchangerate/calc/Calc.java
+++ src/main/java/sk/kosickaakademia/matolak/exchangerate/calc/Calc.java
@@ -18,25 +18,18 @@ public class Calc {
         Set<String> set = new HashSet<>();
         Collections.addAll(set, rates);
 
-        // zavolame metodu getExchangeRates, ktora nam posle vsetky kurzy, ktore pozadujeme
         Api api=new Api();
         Map map = api.getExchangeRates(set);
 
-        // prechadzame polom pozadovanych mien , pre ktore robime konverziu
         for(String temp:rates){
-            // ak dana mena sa nasla a mame aj kurz, robime prepocet
-            if(map.containsKey(temp)){  // temp ... skratka meny, na ktoru robime prevod
-                double value = (double)map.get(temp);  // vymenny kurz
-                double result = eur*value;   // prepocitany vysledok
+            if(map.containsKey(temp)){
+                double value = (double)map.get(temp);
+                double result = eur*value;
 
-                // volame pomocnu metodu ktora vypisuje vysledok
                 print("EUR",temp, eur, result, value);
             }
         }
     }
-
-    // pomocna metoda, sluzi na vypis vysledkov
-    // vypisujeme aj detaily
     private void print(String from, String to, double eur, double result, double rate){
         System.out.println(eur +" "+from+" -> "+result+" "+to+" (exchange rate: "+rate+" )");
     }
